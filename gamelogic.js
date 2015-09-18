37f1b541545a1159ed5ff44f0edab0fdf578d182
Sachin-A <Sachin-A@users.noreply.github.com>
2015-09-18 11:11:35 +0530

    Update gamelogic.js

--- gamelogic.js
+++ gamelogic.js
@@ -59,6 +59,22 @@
     var x;
     var y;
 	
+	//menu navigation variables
+	var escape=false;
+	var instruc=false;
+	var goback=false;
+	var startvar=true;
+	var end=false;
+	var died=false;
+	var countdowndone=false;
+	//var contact=false;
+	var restart=false;
+	var levelnumber=0;
+	var score=0;
+	var highscore;
+	var time;
+	var lives=3;
+	
 	var tempbody = new b2BodyDef();
 	tempbody.type = b2Body.b2_staticBody;
     tempbody.position.Set(0,0);
@@ -82,6 +98,18 @@
     tempfix.shape = new b2CircleShape(0.001 * (xlimit + ylimit) / (1200 + 900));
 	var temp2 = world.CreateBody(tempbody);
     temp2.CreateFixture(tempfix);
+	
+	var tempbody = new b2BodyDef();
+	tempbody.type = b2Body.b2_staticBody;
+    tempbody.position.Set(0,0);
+    tempbody.userData = 'null';
+	var tempfix = new b2FixtureDef();
+	tempfix.density = 10;
+    tempfix.friction = 0.5;
+    tempfix.restitution = 0.6;
+    tempfix.shape = new b2CircleShape(0.001 * (xlimit + ylimit) / (1200 + 900));
+	var portalbody = world.CreateBody(tempbody);
+    portalbody.CreateFixture(tempfix);
 
     //portal variable
     var bcreate = 0;
@@ -107,6 +135,7 @@
     //conveyor and attach variables
     var zero = new b2Vec2(0, 0);
 	var userdata;
+	var userdata2;
 
     //Player: Circle
     //body definition
@@ -136,7 +165,92 @@
     var f1 = new b2Vec2(6 * wheel.GetMass(), 0);
     var f2 = new b2Vec2(0, -12 * wheel.GetMass());
     var f3 = new b2Vec2(-12 * wheel.GetMass(), 0);
-
+	
+	var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_staticBody;
+        bodyDef.position.Set(-1 * xlimit / 1200, (ylimit / scale) - (10 * ylimit / 900));
+        var fd = new b2FixtureDef;
+        fd.shape = new b2PolygonShape;
+        fd.shape.SetAsBox(1 * xlimit / 1200, 20 * ylimit / 900);
+        var border1 = world.CreateBody(bodyDef);
+        border1.CreateFixture(fd);
+		
+		var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_staticBody;
+        bodyDef.position.Set(20 * xlimit / 1200, (ylimit / scale) - (31 * ylimit / 900));
+        var fd = new b2FixtureDef;
+        fd.shape = new b2PolygonShape;
+        fd.shape.SetAsBox(20 * xlimit / 1200, 1* ylimit / 900);
+        var border2 = world.CreateBody(bodyDef);
+        border2.CreateFixture(fd);
+		
+		var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_staticBody;
+        bodyDef.position.Set(41 * xlimit / 1200, (ylimit / scale) - (10 * ylimit / 900));
+        var fd = new b2FixtureDef;
+        fd.shape = new b2PolygonShape;
+        fd.shape.SetAsBox(1 * xlimit / 1200, 20 * ylimit / 900);
+        var border3 = world.CreateBody(bodyDef);
+        border3.CreateFixture(fd);
+		
+		var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_staticBody;
+        bodyDef.position.Set(20 * xlimit / 1200, (ylimit / scale) + (9 * ylimit / 900));
+		bodyDef.userData='death';
+        var fd = new b2FixtureDef;
+        fd.shape = new b2PolygonShape;
+        fd.shape.SetAsBox(20 * xlimit / 1200, 1 * ylimit / 900);
+        var border4 = world.CreateBody(bodyDef);
+        border4.CreateFixture(fd);
+	
+	function createCountDown(timeRemaining) 
+	{
+		var startTime = Date.now();
+		return function() 
+		{
+			if(timeRemaining - ( Date.now() - startTime )<0)
+				return 0;
+			else
+			return timeRemaining - ( Date.now() - startTime );
+		}
+	}
+	
+	function instructions()
+	{
+		document.getElementById('pause').style.display='none';
+		document.getElementById('startmenu').style.display='none';
+		document.getElementById('instructions').style.display='block';
+		instruc=true;
+	}
+	
+	function backtogame()
+	{
+		escape=false;
+	}
+	
+	window.back =function ()
+	{
+		
+		instruc=false;
+		document.getElementById('instructions').style.display='none';
+		if(startvar==true)
+		{
+			document.getElementById('startmenu').style.display='block';
+		}
+		
+	}
+	
+	window.start =function ()
+	{
+		document.getElementById('startmenu').style.display='none';
+		document.getElementById('score').style.display='block';
+		document.getElementById('lives').style.display='block';
+		document.getElementById('time').style.display='block';
+		document.getElementById('canvas').style.display='block';
+		startvar=false;
+		level1();
+	}
+	
     //fps display
     var fps = {
         startTime: 0,
@@ -166,7 +280,7 @@
 		relang=tempbody2.GetAngle()-tempbody.GetAngle()+Math.PI;
 		magx=wheel.GetLinearVelocity().x;
 		magy=wheel.GetLinearVelocity().y;
-		vel.x=-(magx*Math.cos(relang)+magy*Math.sin(relang));
+		vel.x=(magx*Math.cos(relang)+magy*Math.sin(relang));
 		vel.y=-magx*Math.sin(relang)+magy*Math.cos(relang);
 		newpo.x=tempbody.GetPosition().x;
 		newpo.y=tempbody.GetPosition().y;
@@ -174,24 +288,6 @@
 		current=1;
 		}
     }
-
-	var redportal = 
-	{
-        draw: function()
-        {
-            ctx.fillStyle = this.color;
-			ctx.fillRect(temp2.GetPosition().x*scale*xlimit/1200,temp2.GetPosition().y*scale*ylimit/900, 2*scale*xlimit/1200,0.3*scale*ylimit/900 );
-        }
-    };
-	
-    var blueportal = 
-	{
-        draw: function()
-        {
-            ctx.fillStyle = this.color;
-			ctx.fillRect(temp.GetPosition().x*scale*xlimit/1200,temp.GetPosition().y*scale*ylimit/900, 2*scale*xlimit/1200,0.3*scale*ylimit/900 );
-        }
-    };
 	
 	function active()
 	{
@@ -249,6 +345,186 @@
     {
 
     }
+	
+	function draw()
+	{
+		ctx.clearRect ( 0 , 0 , canvas.width , canvas.height );
+		for (b = world.GetBodyList() ; b; b = b.GetNext())
+		{
+			var angle = b.GetAngle()*(180/Math.PI);
+			for(f = b.GetFixtureList(); f; f = f.GetNext()) {
+	 
+			 if (b.GetUserData() == 'obj')
+			 {
+				 var radius = f.GetShape().GetRadius();
+				 var pos = b.GetPosition();
+	 
+				 ctx.save();
+	 
+				 ctx.translate( pos.x * 30, pos.y * 30 );
+				 ctx.rotate( angle * (Math.PI/180) );
+				 ctx.translate( -pos.x * 30, -pos.y * 30 );
+	 
+				 ctx.beginPath();
+				 ctx.arc(pos.x * 30, pos.y * 30, radius * 30, 0, 2 * Math.PI, false);
+				 ctx.closePath();
+				 ctx.lineWidth = 1;
+				 ctx.strokeStyle = "rgba(23, 48, 255, 1)";
+				 ctx.fillStyle = "rgba(23, 48, 255, 0.1)";
+				 ctx.stroke();
+				 ctx.fill();
+	 
+				 ctx.restore();
+	 
+			  }
+			  else if (b.GetUserData() == 'conveyor')
+			 {
+				 var radius = f.GetShape().GetRadius();
+				 var pos = b.GetPosition();
+	 
+				 ctx.save();
+	 
+				 ctx.translate( pos.x * 30, pos.y * 30 );
+				 ctx.rotate( angle * (Math.PI/180) );
+				 ctx.translate( -pos.x * 30, -pos.y * 30 );
+	 
+				 ctx.beginPath();
+				 ctx.arc(pos.x * 30, pos.y * 30, radius * 30, 0, 2 * Math.PI, false);
+				 ctx.closePath();
+				 ctx.lineWidth = 1;
+				 ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+				 ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+				 //ctx.stroke();
+				 ctx.fill();
+	 
+				 ctx.restore();
+	 
+			  }
+	 
+			  else if (b.GetUserData() == 'ground')
+			  { 
+				  var X = f.GetShape().GetVertices()[1].x - f.GetShape().GetVertices()[0].x; 
+				  var Y = f.GetShape().GetVertices()[2].y - f.GetShape().GetVertices()[1].y;    
+				  var pos = b.GetPosition();
+	 
+				  ctx.save();
+	 
+				  ctx.translate( pos.x * 30, pos.y * 30 );
+				  ctx.rotate( angle * (Math.PI/180) );
+				  ctx.translate( -pos.x * 30, -pos.y * 30 );
+	 
+				  ctx.lineWidth = 3;
+				  //ctx.strokeStyle = "rgba(0, 98, 255, 1)";
+				  //ctx.strokeRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+				  ctx.fillStyle = "rgba(0, 125, 255, 0.8)";
+				  ctx.fillStyle = "rgba(0, 20, 29, 1)";
+				  ctx.fillRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+	 
+				  ctx.restore();
+	 
+			   }
+				else if (b.GetUserData() == 'carrier')
+			  { 
+				  var X = f.GetShape().GetVertices()[1].x - f.GetShape().GetVertices()[0].x; 
+				  var Y = f.GetShape().GetVertices()[2].y - f.GetShape().GetVertices()[1].y;    
+				  var pos = b.GetPosition();
+	 
+				  ctx.save();
+	 
+				  ctx.translate( pos.x * 30, pos.y * 30 );
+				  ctx.rotate( angle * (Math.PI/180) );
+				  ctx.translate( -pos.x * 30, -pos.y * 30 );
+	 
+				  ctx.lineWidth = 3;
+				  ctx.strokeStyle = "rgba(25, 29, 39, 1)";
+				  //ctx.strokeRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+				  ctx.fillStyle = "rgba(0, 0, 20, 1)";
+				  ctx.fillRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+	 
+				  ctx.restore();
+	 
+			   } 
+				else if (b.GetUserData() == 'cbox')
+			  { 
+				  var X = f.GetShape().GetVertices()[1].x - f.GetShape().GetVertices()[0].x; 
+				  var Y = f.GetShape().GetVertices()[2].y - f.GetShape().GetVertices()[1].y;    
+				  var pos = b.GetPosition();
+	 
+				  ctx.save();
+	 
+				  ctx.translate( pos.x * 30, pos.y * 30 );
+				  ctx.rotate( angle * (Math.PI/180) );
+				  ctx.translate( -pos.x * 30, -pos.y * 30 );
+	 
+				  ctx.lineWidth = 3;
+				  ctx.strokeStyle = "rgba(200, 77, 82, 1)";
+				  //ctx.strokeRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+				  ctx.fillStyle = "rgba(0, 20, 29, 1)";
+				  ctx.fillRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+	 
+				  ctx.restore();
+	 
+			   }
+			   else if (b.GetUserData() == 'portal')
+			  { 
+				  var X = f.GetShape().GetVertices()[1].x - f.GetShape().GetVertices()[0].x; 
+				  var Y = f.GetShape().GetVertices()[2].y - f.GetShape().GetVertices()[1].y;    
+				  var pos = b.GetPosition();
+	 
+				  ctx.save();
+	 
+				  ctx.translate( pos.x * 30, pos.y * 30 );
+				  ctx.rotate( angle * (Math.PI/180) );
+				  ctx.translate( -pos.x * 30, -pos.y * 30 );
+					
+				  if(b==temp)
+				  {
+					  ctx.strokeStyle = "rgba(0, 0, 255, 1)";
+				      ctx.fillStyle = "rgba(0, 0, 255, 0.8)";
+				  }
+				  else if(b==temp2)
+				  {
+					  ctx.strokeStyle = "rgba(255, 255, 255, 0.8)";
+				      ctx.fillStyle = "rgba(132, 45, 52, 1)";
+				  }
+				  else
+				  {
+					  ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+					  ctx.fillStyle = "rgba(255, 255, 255, 1)";
+				  }
+				  ctx.lineWidth = 1;
+				  
+				  //ctx.strokeRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+				  ctx.fillRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+	 
+				  ctx.restore();
+	 
+			   }
+			   else if (b.GetUserData() == 'bounce')
+			  { 
+				  var X = f.GetShape().GetVertices()[1].x - f.GetShape().GetVertices()[0].x; 
+				  var Y = f.GetShape().GetVertices()[2].y - f.GetShape().GetVertices()[1].y;    
+				  var pos = b.GetPosition();
+	 
+				  ctx.save();
+	 
+				  ctx.translate( pos.x * 30, pos.y * 30 );
+				  ctx.rotate( angle * (Math.PI/180) );
+				  ctx.translate( -pos.x * 30, -pos.y * 30 );
+	 
+				  ctx.lineWidth = 3;
+				  ctx.strokeStyle = "rgba(0, 68, 236, 0.1)";
+				  ctx.strokeRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+				  ctx.fillStyle = "rgba(0, 68, 236, 1)";
+				  ctx.fillRect(((pos.x * 30) - (X * 30 / 2)), ((pos.y * 30) - (Y * 30 / 2)), X * 30, Y * 30);
+	 
+				  ctx.restore();
+	 
+			   }
+			   
+			}
+		}
+	}
 
 	var pull = [];
 	var listener = new Box2D.Dynamics.b2ContactListener;
@@ -270,14 +546,10 @@
 					DUD=temp.GetUserData();
 					if(AUD=='portal' && CUD=='portal' && BUD=='obj' && fbA==temp && rset==1 && bset==1)
 					{
-					//alert("here1");
-					fxB.SetRestitution(1);
                     portals(temp2,temp);
 					}
 					else if(AUD=='portal' && DUD=='portal' && BUD=='obj' && fbA==temp2 && rset==1 && bset==1)
 					{
-					fxB.SetRestitution(1);
-					//alert("here2");
                     portals(temp,temp2);
 					}
 					if(AUD=='bounce' && BUD=='obj')
@@ -297,14 +569,10 @@
 					DUD=temp.GetUserData();
                     if(BUD=='portal' && CUD=='portal' && AUD=='obj' && fbB==temp && rset==1 && bset==1)
 					{
-					fxA.SetRestitution(1);
-					//alert("here3");
 					portals(temp2,temp);
 					}
 					else if(BUD=='portal' && DUD=='portal' && AUD=='obj' && fbB==temp2 && rset==1 && bset==1)
 					{
-					fxA.SetRestitution(1);
-					//alert("here4");
 					portals(temp,temp2);
 					}
 					if(BUD=='bounce' && AUD=='obj')
@@ -319,6 +587,30 @@
 					}
                 }
             }
+			if(!sA && !sB)
+			{
+				if((AUD=='obj' && BUD=='death') || (AUD=='death' && BUD=='obj'))
+				{
+					lives--;
+					died=true;
+					wheel.SetLinearVelocity(zero);
+					wheel.SetAngularVelocity(0);
+				}
+			}
+			if(!sA && !sB)
+			{
+				if((AUD=='ground' && BUD=='obj') || (AUD=='carrier' && BUD=='obj'))
+				{
+					console.log("dfh");
+					listener.BeginContact.localcontact=window.localcontact=true;
+					console.log(contact);
+				}
+				else if((AUD=='obj' && BUD=='ground') || (AUD=='obj' && BUD=='carrier'))
+				{
+					console.log("dfh");
+					listener.BeginContact.localcontact=window.localcontact=true;
+				}
+			}
         }
 	listener.EndContact = function(contact) 
 	{
@@ -337,14 +629,10 @@
 					DUD=temp.GetUserData();
 					if(AUD=='portal' && CUD=='portal' && BUD=='obj' && fbA==temp && rset==1 && bset==1)
 					{
-						fxB.SetRestitution(0.6);
-						//alert("Here0001");
 						current=0;
 					}
 					else if(AUD=='portal' && DUD=='portal' && BUD=='obj' && fbA==temp2 && rset==1 && bset==1)
 					{
-						fxB.SetRestitution(0.6);
-						//alert("Here0002");
 						current=0;
 					}
 					if(AUD=='conveyor' && BUD=='obj')
@@ -360,14 +648,10 @@
 					DUD=temp.GetUserData();
 					if(BUD=='portal' && CUD=='portal' && AUD=='obj' && fbB==temp && rset==1 && bset==1)
 					{
-						fxA.SetRestitution(0.6);
-						//alert("Here0003");
 						current=0;
 					}
 					else if(BUD=='portal' && DUD=='portal' && AUD=='obj' && fbB==temp2 && rset==1 && bset==1)
 					{
-						fxA.SetRestitution(0.6);
-						//alert("Here0004");
 						current=0;
 					}
 					if(BUD=='conveyor' && AUD=='obj')
@@ -377,6 +661,19 @@
 					}
 				 }
 			 }
+			 if(!sA && !sB)
+			{
+				if((AUD=='ground' && BUD=='obj') || (AUD=='carrier' && BUD=='obj')|| (AUD=='bounce' && BUD=='obj'))
+				{
+					console.log("nooo");
+					listener.EndContact.localcontact=window.localcontact=false;
+				}
+				else if((AUD=='obj' && BUD=='ground') || (AUD=='obj' && BUD=='carrier')|| (AUD=='obj' && BUD=='bounce'))
+				{
+					console.log("nooo");
+					listener.EndContact.localcontact=window.localcontact=false;
+				}
+			}
 	}
 	
     function ray()
@@ -403,31 +700,35 @@
                 {
                     closestFraction = output.fraction;
                     intersectionNormal = output.normal;
-					if(bcreate==1 && userdata=='portal' && temp2!=b)
-					{
-						temp=b;
-						bset=1;
-						//portals(temp,temp2);
-						console.log("Blueportal");
-					}
-					if(rcreate==1 && userdata=='portal' && temp!=b)
-					{
-						temp2=b;
-						rset=1;
-						//portals(temp2,temp);
-						console.log("Redportal");
-					}
+					portalbody=b;
                 }
             }
         }
+		userdata2=portalbody.GetUserData();
+		if(bcreate==1 && userdata2=='portal' && temp2!=portalbody)
+		{
+			temp=portalbody;
+			bset=1;
+			console.log("Blueportal");
+		}
+		if(rcreate==1 && userdata2=='portal' && temp!=portalbody)
+		{
+			temp2=portalbody;
+			rset=1;
+		}
         intersectionPoint.x = p1.x + closestFraction * (p2.x - p1.x);
         intersectionPoint.y = p1.y + closestFraction * (p2.y - p1.y);
         normalEnd.x = intersectionPoint.x + intersectionNormal.x;
         normalEnd.y = intersectionPoint.y + intersectionNormal.y;
-        ctx.strokeStyle = "rgba(255, 255, 255, 10)";
+		ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+        ctx.lineWidth = 8;
+        ctx.beginPath();
+        ctx.moveTo(p1.x * scale, p1.y * scale);
+        ctx.lineTo(intersectionPoint.x * scale, intersectionPoint.y * scale);
+        ctx.closePath();
+        ctx.stroke();
+		ctx.strokeStyle = "rgba(255, 255, 255, 1)";
         ctx.lineWidth = 5;
-        ctx.bordercolor = 'red';
-        ctx.borderwidth = 10;
         ctx.beginPath();
         ctx.moveTo(p1.x * scale, p1.y * scale);
         ctx.lineTo(intersectionPoint.x * scale, intersectionPoint.y * scale);
@@ -444,25 +745,6 @@
             case 32:if (count == 0) count = 1;
 					else if (count == 1) count = 0;
 					break;
-            case 37:
-                var mass = wheel.GetMass() * 10;
-                wheel.ApplyImpulse(new b2Vec2(-0.1*mass, 0), wheel.GetPosition());
-                break;
-            case 38:
-                var mass = wheel.GetMass() * 10;
-                wheel.ApplyImpulse(new b2Vec2(0, -0.5*mass), wheel.GetPosition());
-                break;
-            case 39:
-                var mass = wheel.GetMass() * 10;
-                wheel.ApplyImpulse(new b2Vec2(0.1*mass, 0), wheel.GetPosition());
-                break;
-            case 40:
-                var mass = wheel.GetMass() * 10;
-                wheel.ApplyImpulse(new b2Vec2(0, 0.1*mass), wheel.GetPosition());
-                break;
-            case 79:
-                circle = 1;
-                break;
             case 80:
                 if (scope == 0)
                     scope = 1;
@@ -477,31 +759,44 @@
     function upkeyhandler(event)
     {
         var keycode = event.which;
+		var key=event.keyCode;
         switch (keycode)
         {
-            case 79:circle = 0;
+			case 37:if(localcontact==true)
+					{
+						var mass = wheel.GetMass() * 10;
+						wheel.ApplyImpulse(new b2Vec2(-0.2*mass, 0), wheel.GetPosition());
+					}
+                break;
+            case 38:if(localcontact==true)
+					{
+						var mass = wheel.GetMass() * 10;
+						wheel.ApplyImpulse(new b2Vec2(0, -0.5*mass), wheel.GetPosition());
+					}
+                break;
+            case 39:if(localcontact==true)
+					{
+						var mass = wheel.GetMass() * 10;
+						wheel.ApplyImpulse(new b2Vec2(0.2*mass, 0), wheel.GetPosition());
+					}
+                break;
+            default:break;
+        }
+		switch (key)
+        {
+            case 27:if(instruc==false)
+					{
+						if(escape==false)escape=true;
+						else if(escape==true)escape=false;
+					}
 					break;
             default:break;
         }
     }
-
-    var debugDraw = new b2DebugDraw();
-    debugDraw.SetSprite(document.getElementById("canvas").getContext("2d"));
-    debugDraw.SetDrawScale(30); //define scale
-    debugDraw.SetFillAlpha(0.1); //define transparency
-    debugDraw.SetLineThickness(0.1);
-    debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
-    world.SetDebugDraw(debugDraw);
-    var winx = 7 * scale * xlimit / 1200;
-    var winy = ylimit - (19 * scale * ylimit / 900);
-
-    window.onload = function()
-    {
-		redportal.x=0;
-		redportal.y=0;
-		blueportal.x=100;
-		blueportal.y=100;
-		redportal.color='rgba(215, 44, 44, 0.6)';
-		blueportal.color='rgba(54, 161, 255, 0.6)';
-        level1();
-    }
+	var debugDraw = new b2DebugDraw();
+        debugDraw.SetSprite(document.getElementById("canvas").getContext("2d"));
+        debugDraw.SetDrawScale(30);
+        debugDraw.SetFillAlpha(0.5);
+        debugDraw.SetLineThickness(1.0);
+        debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+        world.SetDebugDraw(debugDraw);
