0b7118cd8a295f6482c324b0254ff591c104410d
Sachin-A <Sachin-A@users.noreply.github.com>
2015-09-18 11:10:09 +0530

    Update level3.js

--- level3.js
+++ level3.js
@@ -1,54 +1,26 @@
 function level3()
     {
+		time=180000;
+		levelnumber=3;
+		$("#single2").html("<p>Level: " +levelnumber + "</p>");
+		$("#time").html("Time left: " +time);
+		$("#lives").html("Time left: " +lives);
+		var currentCountDown = createCountDown(time); 
 		
         for (b = world.GetBodyList(); b; b = b.GetNext())
         {
-            if (b != wheel)
+            if (b != wheel && b!=border1 && b!=border2 && b!=border3 && b!=border4)
                 world.DestroyBody(b);
         }
 		var start3=new b2Vec2(4*xlimit/1200,25*ylimit/900);
 		wheel.SetPosition(start3);
-		console.log("Inside level3");
+		
+		var winx3 =13 * scale * xlimit / 1200;
+		var winy3 = ylimit - (19 * scale * ylimit / 900);
 		
 		var mag1=0;
 		var mag2=0;
 		var mag3=0;
-		
-		var bodyDef = new b2BodyDef;
-        bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(-1 * xlimit / 1200, (ylimit / scale) - (10 * ylimit / 900));
-        var fd = new b2FixtureDef;
-        fd.shape = new b2PolygonShape;
-        fd.shape.SetAsBox(1 * xlimit / 1200, 20 * ylimit / 900);
-        var border1 = world.CreateBody(bodyDef);
-        border1.CreateFixture(fd);
-		
-		var bodyDef = new b2BodyDef;
-        bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(20 * xlimit / 1200, (ylimit / scale) - (31 * ylimit / 900));
-        var fd = new b2FixtureDef;
-        fd.shape = new b2PolygonShape;
-        fd.shape.SetAsBox(20 * xlimit / 1200, 1* ylimit / 900);
-        var border2 = world.CreateBody(bodyDef);
-        border2.CreateFixture(fd);
-		
-		var bodyDef = new b2BodyDef;
-        bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(41 * xlimit / 1200, (ylimit / scale) - (10 * ylimit / 900));
-        var fd = new b2FixtureDef;
-        fd.shape = new b2PolygonShape;
-        fd.shape.SetAsBox(1 * xlimit / 1200, 20 * ylimit / 900);
-        var border3 = world.CreateBody(bodyDef);
-        border3.CreateFixture(fd);
-		
-		var bodyDef = new b2BodyDef;
-        bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(20 * xlimit / 1200, (ylimit / scale) + (9 * ylimit / 900));
-        var fd = new b2FixtureDef;
-        fd.shape = new b2PolygonShape;
-        fd.shape.SetAsBox(20 * xlimit / 1200, 1 * ylimit / 900);
-        var border4 = world.CreateBody(bodyDef);
-        border4.CreateFixture(fd);
 		
         // Ground 1
         var bodyDef = new b2BodyDef;
@@ -121,7 +93,7 @@ function level3()
 		bodyDef.position.Set(3 * xlimit / 1200, (ylimit / scale) - (9 * ylimit / 900));
 		bodyDef.userData='conveyor';
 		var fixDef2 = new b2FixtureDef;
-        fixDef2.shape = new b2CircleShape(5);
+        fixDef2.shape = new b2CircleShape(5* xlimit / 1200);
         fixDef2.shape.SetLocalPosition(new b2Vec2(0, 0));
         fixDef2.density = 0;
         fixDef2.isSensor = true;
@@ -156,16 +128,17 @@ function level3()
 		bodyDef.userData='cbox';
         var b1 = new b2FixtureDef;
         b1.shape = new b2PolygonShape();
-        b1.shape.SetAsOrientedBox(2 * ylimit / 900, 0.5 * xlimit / 1200, new b2Vec2(0, 0), (Math.PI/2));
+        b1.shape.SetAsOrientedBox(2 * ylimit / 900, 0.5 * xlimit / 1200, new b2Vec2(0, 0), 0);
         var c2b1 = world.CreateBody(bodyDef);
         c2b1.CreateFixture(b1);
+		c2b1.SetAngle(Math.PI/2);
 		
 		var bodyDef = new b2BodyDef;
         bodyDef.type = b2Body.b2_staticBody;
 		bodyDef.position.Set((xlimit / scale) - (1.5 * xlimit / 1200), (ylimit / scale) - (2 * ylimit / 900));
 		bodyDef.userData='conveyor';
 		var fixDef2 = new b2FixtureDef;
-        fixDef2.shape = new b2CircleShape(5);
+        fixDef2.shape = new b2CircleShape(5* xlimit / 1200);
         fixDef2.shape.SetLocalPosition(new b2Vec2(0, 0));
         fixDef2.density = 0;
         fixDef2.isSensor = true;
@@ -178,36 +151,37 @@ function level3()
 		bodyDef.userData='portal';
 		var fd1 = new b2FixtureDef;
         fd1.shape = new b2PolygonShape;
-        fd1.shape.SetAsOrientedBox(1 * xlimit / 1200, 0.1 * ylimit / 900, new b2Vec2(0, 0), (Math.PI)/2);
+        fd1.shape.SetAsOrientedBox(1 * xlimit / 1200, 0.1 * ylimit / 900, new b2Vec2(0, 0), 0);
         fd1.isSensor = true;
 		var p4= world.CreateBody(bodyDef);
 		p4.CreateFixture(fd1);
+		p4.SetAngle(Math.PI/2);
 
         // intermediate floor
         var bodyDef = new b2BodyDef;
         bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(18 * xlimit / 1200, (ylimit / scale) - (16 * ylimit / 900));
+        bodyDef.position.Set(17 * xlimit / 1200, (ylimit / scale) - (16 * ylimit / 900));
 		bodyDef.userData='ground';
         var floor = new b2FixtureDef;
         floor.shape = new b2PolygonShape();
-        floor.shape.SetAsOrientedBox(18 * xlimit / 1200, 1 * ylimit / 900, new b2Vec2(0, 0), 0);
+        floor.shape.SetAsOrientedBox(17 * xlimit / 1200, 1 * ylimit / 900, new b2Vec2(0, 0), 0);
         var Ifloor = world.CreateBody(bodyDef);
         Ifloor.CreateFixture(floor);
 
         // Second floor
         var bodyDef = new b2BodyDef;
         bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(22 * xlimit / 1200, (ylimit / scale) - (29 * ylimit / 900));
+        bodyDef.position.Set(25 * xlimit / 1200, (ylimit / scale) - (29 * ylimit / 900));
 		bodyDef.userData='carrier';
         var floor2 = new b2FixtureDef;
         floor2.shape = new b2PolygonShape();
-        floor2.shape.SetAsOrientedBox(18 * xlimit / 1200, 1 * ylimit / 900, new b2Vec2(0, 0), 0);
+        floor2.shape.SetAsOrientedBox(15 * xlimit / 1200, 1 * ylimit / 900, new b2Vec2(0, 0), 0);
         var floor2 = world.CreateBody(bodyDef);
         floor2.CreateFixture(floor);
         
 		var bodyDef = new b2BodyDef;
         bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(7 * xlimit / 1200, (ylimit / scale) - (27 * ylimit / 900));
+        bodyDef.position.Set(13 * xlimit / 1200, (ylimit / scale) - (27 * ylimit / 900));
 		bodyDef.userData='cbox';
         var bx1 = new b2FixtureDef;
         bx1.shape = new b2PolygonShape();
@@ -217,7 +191,7 @@ function level3()
 		
 		var bodyDef = new b2BodyDef;
         bodyDef.type = b2Body.b2_staticBody;
-		bodyDef.position.Set(7 * xlimit / 1200, (ylimit / scale) - (27 * ylimit / 900));
+		bodyDef.position.Set(13 * xlimit / 1200, (ylimit / scale) - (27 * ylimit / 900));
 		bodyDef.userData='conveyor';
 		var fixDef2 = new b2FixtureDef;
         fixDef2.shape = new b2CircleShape(6);
@@ -229,7 +203,7 @@ function level3()
         
 		var bodyDef = new b2BodyDef;
         bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(7 * xlimit / 1200, (ylimit / scale) - (25.9 * ylimit / 900));
+        bodyDef.position.Set(13 * xlimit / 1200, (ylimit / scale) - (25.9 * ylimit / 900));
 		bodyDef.userData='portal';
 		var fd1 = new b2FixtureDef;
         fd1.shape = new b2PolygonShape;
@@ -270,7 +244,7 @@ function level3()
                 {
                     if (b.GetPosition().x >= 37 * xlimit / 1200)
                         mag3=-0.1;
-					if(b.GetPosition().x <= 7 * xlimit / 1200)
+					if(b.GetPosition().x <= 13 * xlimit / 1200)
 						mag3=0.1;
                     var movex = b.GetPosition().x + (mag3 * xlimit / 1200);
                     var movey = b.GetPosition().y;
@@ -304,17 +278,41 @@ function level3()
 
         function update3()
         {
-
+			var timeleftgame = currentCountDown();
+			var countDownValue = Math.floor(timeleftgame/1000);
+			$("#time").html("Time left: " +countDownValue);
+			if(countDownValue==0 && restart==false)
+			{
+				lives--;
+				restart=true;
+			}
+			$("#score").html("Score: " +Math.floor(timeleftgame/1000*lives));
+			$("#lives").html("Lives left: " +lives);
+			if(escape==false && end==false)
+			{
+			document.getElementById('pause').style.display='none';
+			document.getElementById('instructions').style.display='none';
+			document.getElementById('canvas').style.backgroundColor='rgba(0, 0, 44, 0.8)';
             world.Step(1 / 60, 10, 10);
-            world.DrawDebugData();
+            draw();
             world.ClearForces();
             world.SetContactListener(listener);
             //fpsa.innerHTML = fps.getFPS();
             conveyor_motion();
 			active();
+			if(died==true)
+				{
+					wheel.SetPosition(start3);
+					died=false;
+				}
 			if(count==1)
 			{
 				attach();
+				ctx.fillStyle = "rgba(255,255,255,0.1)";
+                ctx.beginPath();
+                ctx.arc(wheel.GetPosition().x * scale, wheel.GetPosition().y * scale, 2 * scale, 0, Math.PI * 2, false);
+                ctx.closePath();
+                ctx.fill();
 			}
 			if(shouldmove==1)
 			{
@@ -322,44 +320,47 @@ function level3()
 				wheel.SetLinearVelocity(vel);
 				shouldmove=0;
 			}
-			if(bset>0)
-			{
-				console.log("blue");
-				blueportal.draw();
-			}
-			if(rset>0)
-			{
-				console.log("red");
-				redportal.draw();
-			}
-            if (circle == 1)
-            {
-                ctx.fillStyle = "rgba(255,255,255,0.1)";
-                ctx.beginPath();
-                ctx.arc(wheel.GetPosition().x * scale, wheel.GetPosition().y * scale, 4 * scale, 0, Math.PI * 2, false);
-                ctx.closePath();
-                ctx.fill();
-            }
             if (scope == 1)
             {
                 ray();
             }
             ctx.fillStyle = 'rgba(145,145,255,0.5)';
             ctx.beginPath();
-            ctx.arc(winx, winy, 15 * (xlimit + ylimit) / (1200 + 900), 0, Math.PI * 2, false);
+            ctx.arc(winx3, winy3, 15 * (xlimit + ylimit) / (1200 + 900), 0, Math.PI * 2, false);
             ctx.closePath();
             ctx.fill();
-            if (Math.sqrt(Math.pow(wheel.GetPosition().x * scale - winx, 2) + Math.pow(wheel.GetPosition().y * scale - winy, 2)) > 50)
+			}
+			if(escape==true && instruc==false)
+			{
+				ctx.clearRect(0,0, canvas.width, canvas.height);
+				document.getElementById('canvas').style.backgroundColor='#3366FF';
+				//ctx.fillstyle('#3366FF');
+				//ctx.fillRect(0,0,canvas.width, canvas.height);
+				document.getElementById('pause').style.display='block';
+				//document.getElementById('single').innerHTML="";
+				//document.getElementById('escape').style.display='block';
+				//document.getElementById('escape').innerHTML="You've paused the game";
+			}
+			if(escape==true && goback==true)
+			{
+				document.getElementById('pause').style.display='none';
+				ctx.clearRect(0,0, canvas.width, canvas.height);
+				end=true;
+				window.cancelAnimationFrame(frame1);
+				//level1();
+			}
+            if (Math.sqrt(Math.pow(wheel.GetPosition().x * scale - winx3, 2) + Math.pow(wheel.GetPosition().y * scale - winy3, 2)) > 50 && end==false)
             {
                 var frame3 = window.requestAnimationFrame(update3);
             }
-            else
+            if (Math.sqrt(Math.pow(wheel.GetPosition().x * scale - winx3, 2) + Math.pow(wheel.GetPosition().y * scale - winy3, 2)) < 50 && end==false)
             {
 				wheel.SetLinearVelocity(zero);
 				wheel.SetAngularVelocity(0);
 				rset=0;
 				bset=0;
+				count=0;
                 level4();
             }
-        }
+        };
     }
